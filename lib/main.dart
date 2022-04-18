d3bc961e9796151bd448f6a9a448feb41f5e69aa
asiqur-rahman <17-34907-2@student.aiub.edu>
2022-04-18 09:16:25 +0600

    Initial

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Center(
+      child: Container(
+        child: Text("Hi Asiq"),
+      ),
+    ));
+  }
+}
