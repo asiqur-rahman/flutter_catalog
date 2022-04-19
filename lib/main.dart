891e59b65f72c96b765cea8ac45ee5b57feaf73f
asiqur-rahman <17-34907-2@student.aiub.edu>
2022-04-19 22:53:59 +0600

    day3

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_catalog/homePage.dart';
+import 'pages/homePage.dart';
 
 void main(List<String> args) {
   runApp(MyApp());
@@ -10,6 +10,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     int days = 30;
-    return MaterialApp(home: HomePage());
+    return MaterialApp(
+      home: HomePage(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(
+        brightness: Brightness.light,
+        // primarySwatch: Colors.lightBlue,
+      ),
+      darkTheme: ThemeData(brightness: Brightness.dark),
+    );
   }
 }
